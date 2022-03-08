56b699e9041e96e3c9aabdffc87935581a10e8a3
ffadelm <faysalfadelm0107@gmail.com>
2022-03-08 17:47:58 +0700

    Gender Model Code Documentation

--- Models/Gender.cs
+++ Models/Gender.cs
@@ -7,33 +7,38 @@ using System.ComponentModel.DataAnnotations;
 namespace RentalKendaraan.Models
 {
     /// <summary>
-    /// 
+    /// class partial Gender
     /// </summary>
+    /// <remarks>implementasi fungsionalitas satu class ke semua file</remarks>
     public partial class Gender
     {
         /// <summary>
-        /// 
+        /// method Gender
         /// </summary>
+        /// <remarks>untuk mendukung implementasi set dan menggunakan table hash milik class Customer</remarks>
         public Gender()
         {
             Customers = new HashSet<Customer>();
         }
 
         /// <summary>
-        /// 
+        /// Auto-Property IdGender
         /// </summary>
+        /// <remarks>dapat mengakses data dan info di field private dan biasanya melakukannya dari property public</remarks>
         public int IdGender { get; set; }
 
         /// <summary>
-        /// 
+        /// auto property NamaGender bertipe String
         /// </summary>
+        /// <remarks>agar dapat mengakses data dan info di field private dan biasanya melakukannya dari property public</remarks>
         [MaxLength(1, ErrorMessage = "Isi dengan L: Laki-Laki & P: Perempuan")]
         [Required(ErrorMessage = "Jenis kelamin tidak boleh kosong")]
         public string NamaGender { get; set; }
 
         /// <summary>
-        /// 
+        /// method virtual Customer
         /// </summary>
+        /// <remarks>agar dapat di akses oleh class lain sehingga dibuat auto property</remarks>
         public virtual ICollection<Customer> Customers { get; set; }
     }
 }
