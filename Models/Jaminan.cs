f4802c3d99960fe1098734ea9b88ec980bd119dd
ffadelm <faysalfadelm0107@gmail.com>
2022-03-08 18:16:55 +0700

    Jaminan Model Code Documentation

--- Models/Jaminan.cs
+++ Models/Jaminan.cs
@@ -6,18 +6,38 @@ using System.ComponentModel.DataAnnotations;
 
 namespace RentalKendaraan.Models
 {
+    /// <summary>
+    /// Class Partial Jaminan
+    /// </summary>
+    /// <remarks>implementasi fungsionalitas satu class ke semua file</remarks>
     public partial class Jaminan
     {
+        /// <summary>
+        /// Method Jaminan
+        /// </summary>
+        /// <remarks>untuk mendukung implementasi set dan menggunakan table hash milik class peminjaman</remarks>
         public Jaminan()
         {
             Peminjamen = new HashSet<Peminjaman>();
         }
 
+        /// <summary>
+        /// Auto-Property IdJaminan
+        /// </summary>
+        /// <remarks>dapat mengakses data dan info di field private dan biasanya melakukannya dari property public</remarks>
         public int IdJaminan { get; set; }
 
-        [Required(ErrorMessage = "Janminan tidak boleh kosong")]
+        /// <summary>
+        /// auto property NamaJaminan bertipe String
+        /// </summary>
+        /// <remarks>agar dapat mengakses data dan info di field private dan biasanya melakukannya dari property public</remarks>
+        [Required(ErrorMessage = "Jaminan tidak boleh kosong")]
         public string NamaJaminan { get; set; }
 
+        /// <summary>
+        /// method virtual Peminjaman
+        /// </summary>
+        /// <remarks>supaya dapat di akses oleh class lain sehingga dibuat auto property</remarks>
         public virtual ICollection<Peminjaman> Peminjamen { get; set; }
     }
 }
