81fdf98a760ea802cac5d8afcdaf787268a88947
ffadelm <faysalfadelm0107@gmail.com>
2022-03-08 18:29:53 +0700

    Kondisi Kendaraan model Code Documentation

--- Models/KondisiKendaraan.cs
+++ Models/KondisiKendaraan.cs
@@ -6,18 +6,38 @@ using System.ComponentModel.DataAnnotations;
 
 namespace RentalKendaraan.Models
 {
+    /// <summary>
+    /// Class Partial Kondisi Kendaraan
+    /// </summary>
+    /// <remarks>implementasi fungsionalitas satu class ke semua file</remarks>
     public partial class KondisiKendaraan
     {
+        /// <summary>
+        /// Method Kondisi Kendaraan
+        /// </summary>
+        /// <remarks>untuk mendukung implementasi set dan menggunakan table hash milik class pengembalian</remarks>
         public KondisiKendaraan()
         {
             Pengembalians = new HashSet<Pengembalian>();
         }
 
+        /// <summary>
+        /// Auto-Property Id Kondisi
+        /// </summary>
+        /// <remarks>dapat mengakses data dan info di field private dan biasanya melakukannya dari property public</remarks>
         public int IdKondisi { get; set; }
 
+        /// <summary>
+        /// auto property Nama Kondisi bertipe String
+        /// </summary>
+        /// <remarks>agar dapat mengakses data dan info di field private dan biasanya melakukannya dari property public</remarks>
         [Required(ErrorMessage = "Kondisi Kendaraan tidak boleh kosong")]
         public string NamaKondisi { get; set; }
 
+        /// <summary>
+        /// Method virtual Pengembalian
+        /// </summary>
+        /// <remarks>supaya dapat di akses oleh class lain sehingga dibuat auto property</remarks>
         public virtual ICollection<Pengembalian> Pengembalians { get; set; }
     }
 }
