6e9afd7949890eedb3c75e7a1634a61a0295ae00
ffadelm <faysalfadelm0107@gmail.com>
2022-03-08 17:31:53 +0700

    ErrorView Model  Code Documentation

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -3,18 +3,21 @@ using System;
 namespace RentalKendaraan.Models
 {
     /// <summary>
-    /// 
+    /// main class ErrorViewModel
     /// </summary>
+    /// <remarks>membuat class Handling Error </remarks>
     public class ErrorViewModel
     {
         /// <summary>
-        /// 
+        /// method auto-property  Request Id
         /// </summary>
+        /// <remarks>sebagai method untuk dapat di akses oleh field lain</remarks>
         public string RequestId { get; set; }
 
         /// <summary>
-        /// 
+        /// method showRequestID
         /// </summary>
+        /// <remarks>digunakan sebagai pengecekan pengkondisian apakah ad id yang Request atau tidak</remarks>
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
